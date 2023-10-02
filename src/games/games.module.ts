a2b14c428b04a7297b7a198cd67cbf21336e1754
Amanda Xavier <amandavxavier@gmail.com>
2023-10-01 21:59:02 -0300

    Feat: updating providers module with repository's files

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { GamesController } from './games.controller';
+import { GamesRepository } from './games.repository';
 
 @Module({
   controllers: [GamesController],
-  providers: [GamesService],
+  providers: [GamesService, GamesRepository],
 })
 export class GamesModule {}
--- src/participants/participants.module.ts
+++ src/participants/participants.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ParticipantsService } from './participants.service';
 import { ParticipantsController } from './participants.controller';
+import { ParticipantsRepository } from './participants.repository';
 
 @Module({
   controllers: [ParticipantsController],
-  providers: [ParticipantsService],
+  providers: [ParticipantsService, ParticipantsRepository],
 })
 export class ParticipantsModule {}
