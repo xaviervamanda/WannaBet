1f375fc69aede536651fef46f04da4b3e32f612e
Amanda Xavier <amandavxavier@gmail.com>
2023-10-02 23:28:00 -0300

    Fix: dinamize api port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log("Server is up and running on port: " + port);
+  });
 }
 bootstrap();
