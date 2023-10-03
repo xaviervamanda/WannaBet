17acccb8fdfb70a78d3a151bb94d5e51399878ad
Amanda Xavier <amandavxavier@gmail.com>
2023-10-02 23:47:27 -0300

    Feat: prisma seed and scripts package.json

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const participant = await prisma.participant.create({
+        data: {
+            name: "John Doe",
+            balance: 200,
+        },
+    });
+
+    const game = await prisma.game.create({
+        data: {
+            homeTeamName: "Avai",
+            awayTeamName: "Botafogo",   
+        }
+    })
+
+    const bet = await prisma.bet.create({
+        data: {
+            participantId: participant.id,
+            gameId: game.id,
+            homeTeamScore: 2,
+            awayTeamScore: 1,
+            amountBet: 100
+        },
+    })
+
+    console.log({ participant, game, bet });
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    })
