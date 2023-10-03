e8a2dc9a0d86de933c01c5e366d79c3bb4854ec0
Amanda Xavier <amandavxavier@gmail.com>
2023-10-03 17:12:10 -0300

    Docs: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wanna Bet? - Rest API')
+    .setDescription('Wanna Bet API description')
+    .setVersion('1.0')
+    // .addTag('Driven Pass')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log("Server is up and running on port: " + port);
